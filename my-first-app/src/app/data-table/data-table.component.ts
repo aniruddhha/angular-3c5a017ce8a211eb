0ebba31d908b4cd83e8472ec86eff81fecb672ed
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-22 21:51:00 +0530

    component : ngOnInit method

--- my-first-app/src/app/data-table/data-table.component.ts
+++ my-first-app/src/app/data-table/data-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TableData } from './table-data';
 
 @Component({
   selector: 'app-data-table',
@@ -7,19 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataTableComponent implements OnInit {
 
-  invoices = [
-    { invNo: '1223489', date: '15 Jul 2021', client: 'abc', invAmt: 45, taxAmt: 8 },
-    { invNo: '1226669', date: '16 Jul 2021', client: 'pqr', invAmt: 100, taxAmt: 10 },
-    { invNo: '1236886', date: '17 Jul 2021', client: 'xyz', invAmt: 30, taxAmt: 2 },
-    { invNo: '1239862', date: '18 Jul 2021', client: 'tuv', invAmt: 1000, taxAmt: 30 },
-    { invNo: '1244258', date: '19 Jul 2021', client: 'ijk', invAmt: 10, taxAmt: 1 },
-    { invNo: '1245558', date: '20 Jul 2021', client: 'lmn', invAmt: 54, taxAmt: 3 },
-    { invNo: '1265346', date: '21 Jul 2021', client: 'hij', invAmt: 899, taxAmt: 20 }
-  ]
+  company: string
+  invoices: Array<TableData>
 
-  constructor() { }
+  constructor(
+    // this feature of TS and not of JS
+    // here is public is access modifier
+    // access modifier is that thing which restricts access,
+  ) {
+    this.company = 'my company'
+    this.invoices = [
+      { invNo: null, date: null, client: null, invAmt: null, taxAmt: null },
+      { invNo: '1223489', date: '15 Jul 2021', client: 'abc', invAmt: 45, taxAmt: 8 },
+      { invNo: '1226669', date: '16 Jul 2021', client: 'pqr', invAmt: 100, taxAmt: 10 },
+      { invNo: '1236886', date: '17 Jul 2021', client: 'xyz', invAmt: 30, taxAmt: 2 },
+      { invNo: '1239862', date: '18 Jul 2021', client: 'tuv', invAmt: 1000, taxAmt: 30 },
+      { invNo: '1244258', date: '19 Jul 2021', client: 'ijk', invAmt: 10, taxAmt: 1 },
+      { invNo: '1245558', date: '20 Jul 2021', client: 'lmn', invAmt: 54, taxAmt: 3 },
+      { invNo: '1265346', date: '21 Jul 2021', client: 'hij', invAmt: 899, taxAmt: 20 }
+    ]
+  } // component object would be getting created
 
-  ngOnInit(): void {
+  ngOnInit(): void { // from this line object will be treated as Angular Component
+    // e.g.
+    // go to web and pull the data 
+    // subscribe to anything 
+    // initilization of state can be done here
+    // etc etc 
   }
-
 }
---
+++ my-first-app/src/app/data-table/table-data.ts
@@ -0,0 +1,7 @@
+export interface TableData {
+    invNo: string | null,
+    date: string | null,
+    client: string | null,
+    invAmt: number | null,
+    taxAmt: number | null
+}
