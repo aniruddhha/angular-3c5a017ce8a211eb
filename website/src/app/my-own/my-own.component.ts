df6dfee6debcaade2018a5bab526f19f4ebe55bb
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-23 23:19:04 +0530

    dom manipulation

--- website/src/app/my-own/my-own.component.ts
+++ website/src/app/my-own/my-own.component.ts
@@ -10,6 +10,9 @@ export class MyOwnComponent implements OnInit {
   @ViewChild('nm', { static: true })
   nm!: ElementRef
 
+  @ViewChild('btn', { static: true })
+  btn!: ElementRef<HTMLButtonElement>
+
   isSh = false
 
   constructor() { }
@@ -18,6 +21,8 @@ export class MyOwnComponent implements OnInit {
     console.log(this)
     console.log(this.nm)
     this.nm.nativeElement.style.border = '1px solid red'
+
+    setTimeout(() => this.btn.nativeElement.click(), 5000)
   }
 
   onDropdownClicked() {
