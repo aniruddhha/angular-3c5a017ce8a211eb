1fb26917199bfc640a5ec2fc3843599d2ccbc17d
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-23 20:16:06 +0530

    website : design converting to component

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebHeaderComponent } from './web-header/web-header.component';
+import { WebMenuComponent } from './web-menu/web-menu.component';
+import { WebMainComponent } from './web-main/web-main.component';
+import { FeaturedBlogComponent } from './web-main/featured-blog/featured-blog.component';
+import { BlogPostComponent } from './web-main/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebHeaderComponent,
+    WebMenuComponent,
+    WebMainComponent,
+    FeaturedBlogComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
---
+++ website/src/app/web-menu/web-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WebMenuComponent } from './web-menu.component';
+
+describe('WebMenuComponent', () => {
+  let component: WebMenuComponent;
+  let fixture: ComponentFixture<WebMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WebMenuComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WebMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
