7ee2a470ac08b4f0be0942b7f7525bf8eb6a47bb
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-26 23:17:31 +0530

    @input and @output

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -8,10 +8,16 @@ import { DataService } from './data.service';
 })
 export class AppComponent {
   title = 'my-first-app';
+  cl = ''
 
   constructor(
 
   ) {
 
+  }
+
+  clkEv(cl: string) {
+    console.log('event came out')
+    this.cl = cl
   }
 }
--- my-first-app/src/app/op/op.component.ts
+++ my-first-app/src/app/op/op.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-op',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OpComponent implements OnInit {
 
+  @Output('clk')
+  em = new EventEmitter()
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onClk() {
+    console.log(`Button Clicked`)
+    this.em.emit('blue')
+  }
 }
