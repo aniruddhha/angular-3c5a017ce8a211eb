344ea4e17412d90f75252847bb1a3e3429940da9
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-28 21:19:27 +0530

    routing : child routes

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './dashboard/customers/customers.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { InvoicesComponent } from './dashboard/invoices/invoices.component';
+import { OrdersComponent } from './dashboard/orders/orders.component';
+import { ProductsComponent } from './dashboard/products/products.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+
+const dashRoutes: Routes = [
+  { path: 'invoices', component: InvoicesComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'customers', component: CustomersComponent }
+]
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', redirectTo: '' },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, children: dashRoutes },
   { path: '**', component: NotFoundComponent }
 ];
 
--- routing-app/src/app/app.module.ts
+++ routing-app/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
+import { InvoicesComponent } from './dashboard/invoices/invoices.component';
+import { OrdersComponent } from './dashboard/orders/orders.component';
+import { ProductsComponent } from './dashboard/products/products.component';
+import { CustomersComponent } from './dashboard/customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    InvoicesComponent,
+    OrdersComponent,
+    ProductsComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
