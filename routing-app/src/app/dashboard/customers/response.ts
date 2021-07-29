67d5abfdf2889da1665e413076ca9394b116d449
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-07-29 20:53:03 +0530

    http : json to ts

---
+++ routing-app/src/app/dashboard/customers/response.ts
@@ -0,0 +1,21 @@
+export interface AppRes {
+    page: number;
+    per_page: number;
+    total: number;
+    total_pages: number;
+    data: Array<CustData>;
+    support: Support
+}
+
+export interface CustData {
+    id: number;
+    name: string;
+    year: number;
+    color: string;
+    pantone_value: string;
+}
+
+export interface Support {
+    url: string;
+    text: string;
+}
